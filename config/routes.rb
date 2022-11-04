7fa1f4e5a11148db9f6c4ef39d187d24d8aa7002
LoiseauB <bapt.loiseau@gmail.com>
2022-11-04 16:44:05 +0100

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bottles do
+    resources :likes, only: [:create, :destroy]
+    resources :comments
+    resources :awards, only: [:create, :show, :edit]
+  end
 end
